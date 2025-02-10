52a7a113eed3f01434d91c0f86b588f2822933bc
Jonathan130900 <jlb130900@gmail.com>
2025-02-10 14:37:34 +0100

    Program.cs impostato, inizio configurazione classi

--- OOP_Esercizio_1/Program.cs
+++ OOP_Esercizio_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP_Esercizio_1;
+
+var athlete = new Athlete("Randy", "'The Legend Killer'", "Orton", 44, "Wrestler", "WWE");
+
+var employee = new Employee("Gabe", "'Gaben'", "Newell", 62, "Video game developer and businessman", "Valve");
+
+var animal = new Animal("Ham", "'Ham the Astrochimp'", "No 65", "Chimpanzee", "Project Mercury", "Museum of Space History New Mexico");
+
+var vehicle = new Vehicle("DMC DeLorean", "DeLorean Motor Company", 1981, 1983, "Giogetto Giugiaro");
+
+
+
