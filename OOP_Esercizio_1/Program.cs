770777e910158702e38792141dd30361a94525e3
Jonathan130900 <jlb130900@gmail.com>
2025-02-10 14:56:08 +0100

    Classe Employee fatta

--- OOP_Esercizio_1/Program.cs
+++ OOP_Esercizio_1/Program.cs
@@ -1,14 +1,15 @@
 ﻿using OOP_Esercizio_1;
 
-var athlete = new Athlete("Randy", "'The Legend Killer'", "Orton", 44, "Professional Wrestler", "WWE");
+var athlete = new Athlete("Randy", "'The Legend Killer'", "Orton", 44, "professional wrestler", "WWE");
 
-var employee = new Employee("Gabe", "'Gaben'", "Newell", 62, "Video game developer and businessman", "Valve");
+var employee = new Employee("Gabe", "'Gaben'", "Newell", 62, "video game developer and businessman", "Valve");
 
-var animal = new Animal("Ham", "'Ham the Astrochimp'", "No 65", "Chimpanzee", "Project Mercury", "Museum of Space History New Mexico");
+var animal = new Animal("Ham", "'Ham the Astrochimp'", "No 65", "chimpanzee", "Project Mercury", "Museum of Space History New Mexico");
 
 var vehicle = new Vehicle("DMC DeLorean", "DeLorean Motor Company", 1981, 1983, "Giogetto Giugiaro");
 
 
 athlete.showAthleteData();
+employee.showEmployeeData();
 
 
